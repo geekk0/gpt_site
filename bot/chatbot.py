6874d8e0a0d27ad2d00974e83a59a04081131df4
gekk0 <gekk0dw@gmail.com>
2023-04-16 15:09:25 +0300

    first

--- bot/chatbot.py
+++ bot/chatbot.py
@@ -3,10 +3,12 @@ import openai
 
 def get_gpt_resp(prompt):
     openai.api_key = "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"
-    model_engine = "text-davinci-003"
+
+    """model_engine = "text-davinci-003"
 
     completion = openai.Completion.create(
         engine=model_engine,
+        # model="gpt-4",
         prompt=prompt,
         max_tokens=1024,
         n=1,
@@ -16,8 +18,17 @@ def get_gpt_resp(prompt):
 
     # print(completion)
 
-    response = completion.choices[0].text
+    response = completion.choices[0].text"""
+
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                            messages=[
+                                                {"role": "user", "content": prompt}],
+                                            temperature=0.5,
+                                            max_tokens=200)
+                                            # n=1,
+                                            # stop=None
+                                            # seed=i)
 
 
-    return response
+    return response.choices[0].message['content']
 
